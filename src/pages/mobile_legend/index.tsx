3598a0c0a9fc7827a14cb2ef0306e190a7ab66d9
azra <azrayazidalkautsar@gmail.com>
2024-01-19 00:16:42 +0700

    update mobile legend

--- src/pages/mobile_legend/index.tsx
+++ src/pages/mobile_legend/index.tsx
@@ -6,26 +6,29 @@ interface Hero {
   id: string;
   mlid: string;
   hero_name: string;
-  role: string;
-  hero_icon: string;
+  hero_role: string;
+  hero_specially: string;
+  hero_avatar: string;
 }
 
 export default function MobileLegend() {
   const [search, setSearch] = useState("");
   const [heroes, setHeroes] = useState<Hero[]>([]);
-  const [results, setResults] = useState([]);
   const [searchInfo, setSearchInfo] = useState({});
 
-  const handleSearch = async () => {
-    // e.preventDefault();
+  const handleSearch = async (e: any) => {
+    e.preventDefault();
     await axios
-      .get(`https://api.dazelpro.com/mobile-legends/hero/{:id}`)
+      .get(`https://api.dazelpro.com/mobile-legends/hero?heroName=${setSearch}`)
       .then((response) => {
+        // console.log("data search", response.data.hero);
         setSearchInfo(response);
       })
       .catch((error) => {
         console.log(error);
       });
+
+    console.log(search);
   };
 
   const getDataHeroes = async () => {
@@ -41,19 +44,21 @@ export default function MobileLegend() {
   };
 
   useEffect(() => {
-    // testApi();
-    handleSearch();
+    const mockEvent = { preventDefault: () => {} };
+    handleSearch(mockEvent);
     getDataHeroes();
   }, []);
 
   return (
     <>
+      <h2 className="text-xl font-bold mb-2">MOBILE LEGEND</h2>
+
       <div className="mb-3 xl:w-1/3 w-80 justify-center">
         <label className="relative block">
           <form onSubmit={handleSearch}>
             <input
               className="rounded-full placeholder:italic placeholder:text-slate-400 block bg-slate-50 w-full border border-slate-300 py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-lg"
-              placeholder="Coba kocok..."
+              placeholder="Search Here"
               type="search"
               name="search"
               value={search}
@@ -64,11 +69,14 @@ export default function MobileLegend() {
       </div>
 
       <div className="flex justify-center">
-        <main className="list-film py-3 ">
+        <main className="list-film py-3">
           <div className="container-fluid">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
-              {heroes &&
-                heroes.map((hero) => (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+              {heroes
+                .filter((hero) =>
+                  hero.hero_name.toLowerCase().includes(search.toLowerCase())
+                )
+                .map((hero) => (
                   <div
                     className="bg-white p-6 rounded-md shadow-md"
                     key={hero.id}
@@ -76,7 +84,10 @@ export default function MobileLegend() {
                     <h2 className="text-xl font-bold mb-2">
                       Name: {hero.hero_name}
                     </h2>
-                    <p className="text-gray-600">Role: {hero.role}</p>
+                    <p className="text-gray-600">Role: {hero.hero_role}</p>
+                    <p className="text-gray-600">
+                      Speciality: {hero.hero_specially}
+                    </p>
                   </div>
                 ))}
             </div>
