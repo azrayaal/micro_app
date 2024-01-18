d6135bc1c18f9f7e78b4131b26e8bc096f452ae3
azra <azrayazidalkautsar@gmail.com>
2024-01-18 23:43:41 +0700

    update

--- src/pages/mobile_legend/index.tsx
+++ src/pages/mobile_legend/index.tsx
@@ -1,24 +1,88 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import dataHero from "./component_ml/hero-meta-final";
+
+interface Hero {
+  id: string;
+  mlid: string;
+  hero_name: string;
+  role: string;
+  hero_icon: string;
+}
+
 export default function MobileLegend() {
+  const [search, setSearch] = useState("");
+  const [heroes, setHeroes] = useState<Hero[]>([]);
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+
+  const handleSearch = async () => {
+    // e.preventDefault();
+    await axios
+      .get(`https://api.dazelpro.com/mobile-legends/hero/{:id}`)
+      .then((response) => {
+        setSearchInfo(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const getDataHeroes = async () => {
+    await axios
+      .get(`https://api.dazelpro.com/mobile-legends/hero`)
+      .then((response) => {
+        // console.log("data herodata ml", response.data);
+        setHeroes(response.data.hero);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    // testApi();
+    handleSearch();
+    getDataHeroes();
+  }, []);
+
   return (
-    <div className="flex justify-center">
-      <div className="mb-3 xl:w-1/3 w-80">
+    <>
+      <div className="mb-3 xl:w-1/3 w-80 justify-center">
         <label className="relative block">
-          <form>
-            {/* <form onSubmit={handleSearch}> */}
-            {/* <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-            <box-icon name="search-alt-2" color="#5c5c5c"></box-icon>
-          </span> */}
+          <form onSubmit={handleSearch}>
             <input
               className="rounded-full placeholder:italic placeholder:text-slate-400 block bg-slate-50 w-full border border-slate-300 py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-lg"
               placeholder="Coba kocok..."
               type="search"
               name="search"
-              // value={search}
-              // onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
             />
           </form>
         </label>
       </div>
-    </div>
+
+      <div className="flex justify-center">
+        <main className="list-film py-3 ">
+          <div className="container-fluid">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+              {heroes &&
+                heroes.map((hero) => (
+                  <div
+                    className="bg-white p-6 rounded-md shadow-md"
+                    key={hero.id}
+                  >
+                    <h2 className="text-xl font-bold mb-2">
+                      Name: {hero.hero_name}
+                    </h2>
+                    <p className="text-gray-600">Role: {hero.role}</p>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
